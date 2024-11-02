7689cb6402c12b87941e502678688c55ec76e11d
Evelyn <shuxinli@andrew.cmu.edu>
2024-11-01 20:17:47 -0400

    fix: modifed foreign key for course and agreement model

--- ntn_app/models.py
+++ ntn_app/models.py
@@ -204,7 +204,7 @@ class UniversityProfile(models.Model):
     marriage_or_kids_support = models.TextField(blank=True, null=True)
     vets_support = models.TextField(blank=True, null=True)
     aids_or_scholarships_for_transfers = models.TextField(blank=True, null=True)
-    special_transfer_program = models.TextField(blank=True, null=True)
+    special_transfer_support = models.TextField(blank=True, null=True)
     contigent_admission = models.TextField(blank=True, null=True)
     online_allowed = models.BooleanField(default=False)
     degree_pathways_for_AAS_student = models.TextField(blank=True, null=True)
@@ -235,9 +235,9 @@ class CollegeProfile(models.Model):
     full_time_students = models.IntegerField(blank=True, null=True)
     part_time_students = models.IntegerField(blank=True, null=True)
     HS_dual_enrollment = models.BooleanField(default=False)
-    on_site_HS_available = models.BooleanField(default=False)
+    on_site_HS_available = models.BooleanField(default=False, blank=True)
     number_of_transfers_to_univerisities = models.IntegerField(blank=True, null=True)
-    honors_program = models.BooleanField(default=False)
+    honors_program = models.BooleanField(default=False, blank=True)
     enrolled_honors_students = models.IntegerField(blank=True, null=True)
     top1_transfer_partner = models.CharField(max_length=100, blank=True, null=True)
     top2_transfer_partner = models.CharField(max_length=100, blank=True, null=True)
@@ -253,7 +253,7 @@ class CollegeProfile(models.Model):
     direct_grad_pathways = models.TextField(blank=True, null=True)
     honor_to_honor_pathways = models.TextField(blank=True, null=True)
     college_strength = models.TextField(blank=True, null=True)
-    housing_for_international_students = models.BooleanField(default=False)
+    housing_for_international_students = models.BooleanField(default=False, blank=True)
     sports_teams = models.TextField(blank=True, null=True)
     # description = models.TextField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -271,7 +271,9 @@ class UniversityUser(models.Model):
 
     # Ensure that a user can only belong to an institution once
     class Meta:
-        unique_together = ('user', 'university') 
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'university'], name='unique_university_user')
+        ]
 
     def __str__(self):
         return f"{self.user.username} at {self.university}"
@@ -285,23 +287,34 @@ class CollegeUser(models.Model):
 
     # Ensure that a user can only belong to an institution once
     class Meta:
-        unique_together = ('user', 'college') 
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'college'], name='unique_college_user')
+        ]
 
     def __str__(self):
         return f"{self.user.username} at {self.college}"
 
 # Represents an agreement between two institutions
 class Agreement(models.Model):
-    university = models.ForeignKey(InstitutionProfile, limit_choices_to={'institution_type': 'university'}, on_delete=models.CASCADE)
-    college = models.ForeignKey(InstitutionProfile, limit_choices_to={'institution_type': 'college'}, on_delete=models.CASCADE)
+    university = models.ForeignKey(UniversityProfile, on_delete=models.CASCADE)
+    college = models.ForeignKey(CollegeProfile, on_delete=models.CASCADE)
     effective_term = models.DateField()
 
     def __str__(self):
         return f"Agreement between {self.university} and {self.college}"
 
 # Represents a course 
-class Course(models.Model):
-    school = models.ForeignKey(InstitutionProfile, on_delete=models.CASCADE)
+class UniversityCourse(models.Model):
+    institution = models.ForeignKey(UniversityProfile, on_delete=models.CASCADE)
+    subject_code = models.CharField(max_length=10)
+    digit_code = models.CharField(max_length=10)
+    credits = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.subject_code} {self.digit_code}"
+    
+class CollegeCourse(models.Model):
+    institution = models.ForeignKey(CollegeProfile, on_delete=models.CASCADE)
     subject_code = models.CharField(max_length=10)
     digit_code = models.CharField(max_length=10)
     credits = models.IntegerField()
@@ -312,11 +325,11 @@ class Course(models.Model):
 # Represents a course equivalency between two institutions
 class AgreementCourse(models.Model):
     agreement = models.ForeignKey(Agreement, on_delete=models.CASCADE)
-    course_1 = models.ForeignKey(Course, related_name='agreement_course_1', on_delete=models.CASCADE)
-    course_2 = models.ForeignKey(Course, related_name='agreement_course_2', on_delete=models.CASCADE)
+    college_course = models.ForeignKey(CollegeCourse, on_delete=models.CASCADE)
+    university_course = models.ForeignKey(UniversityCourse, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.course_1} ↔ {self.course_2} in {self.agreement}"
+        return f"{self.college_course} ↔ {self.university_course} in {self.agreement}"
     
 # Represents a student profile
 class StudentProfile(models.Model):
@@ -339,7 +352,7 @@ class StudentProfile(models.Model):
     state = models.CharField(max_length=2, choices=STATE_CHOICES, blank=True, null=True)
     zipcode = models.CharField(max_length=10, blank=True, null=True)
     citizenship_status = models.CharField(max_length=20, choices=CITIZENSHIP_CHOICES, blank=True, null=True)
-    is_enrolled_in_college = models.BooleanField(default=False, blank=True, null=True)
+    is_enrolled_in_college = models.BooleanField(default=False, blank=True)
     major = models.CharField(max_length=100, blank=True, null=True)
     gpa = models.CharField(max_length=10, blank=True, null=True)
     college_credits_num = models.IntegerField(blank=True, null=True)
@@ -349,9 +362,9 @@ class StudentProfile(models.Model):
     location_preference = models.CharField(max_length=20, choices=LOCATION_PREFERENCE_CHOICES, blank=True, null=True)
     size_preference = models.CharField(max_length=20, choices=SIZE_PREFERENCE_CHOICES, blank=True, null=True)
     degree_preference = MultiSelectField(choices=DEGREE_CHOICES, blank=True)
-    graduate_pathway_preference = models.BooleanField(default=False, blank=True, null=True)
+    graduate_pathway_preference = models.BooleanField(default=False, blank=True)
     career_objective = models.TextField(blank=True, null=True)
-    is_fafsa_completed = models.BooleanField(default=False, blank=True, null=True)
+    is_fafsa_completed = models.BooleanField(default=False, blank=True)
     expected_family_contribution = models.IntegerField(blank=True, null=True)
     estimated_affordable_amount = models.IntegerField(blank=True, null=True)
     
@@ -361,7 +374,7 @@ class StudentProfile(models.Model):
 # Represents the courses a student has taken
 class StudentCourse(models.Model):
     student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.ForeignKey(CollegeCourse, on_delete=models.CASCADE)
     grade = models.CharField(max_length=5)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
